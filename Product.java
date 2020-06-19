cc8071077325ce664f01ed4f26f08ef040ef4e74
harshitaaldak <50993984+harshitaaldak@users.noreply.github.com>
2020-06-20 05:02:07 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,60 @@
+package com.products;
+import java.util.Scanner;
+
+public class Product {
+
+	public static void main(String[] args) {		
+			Scanner scanner= new Scanner(System.in);
+		
+			double totalCost=0.0 ,finalTax_A=0.0,finalTax_B=0.0;
+			
+			double Tax =0.0,salestax_A = 0.0, salestax_B = 0.0;
+			
+			double cost_A = 0.0 ,cost_B = 0.0,cost_C=0.0;
+			
+			int items=scanner.nextInt();
+	if (items<=0)
+    {
+			System.out.println("Invalid Input");
+			System.exit(0);
+     }
+		
+	else
+	{
+		for(int i=0;i<items;i++)
+		{
+			double costprice = scanner.nextDouble();
+		    String category=scanner.next();
+				
+				if(category.equals("A"))
+				{
+					salestax_A = costprice*0.1;
+					cost_A = salestax_A +costprice+cost_A;
+					finalTax_A = salestax_A+finalTax_A;
+				}
+				else if(category.equals("B"))
+				{
+					salestax_B = costprice*0.2;
+					cost_B=salestax_B +costprice+cost_B;
+					finalTax_B=salestax_B+finalTax_B;
+				}
+				else if(category.equals("C"))
+				{ 
+					cost_C=costprice+cost_C ;
+				}
+				else
+				{
+					System.out.println("Invalid Input");
+					System.exit(0);
+				}
+		}
+					
+				totalCost =cost_A +cost_B+cost_C ;
+				Tax=finalTax_A+finalTax_B ;
+				
+				System.out.println("TOTAL COST "+totalCost);
+		        System.out.println("TOTAL SALES TAX "+Tax);
+		        scanner.close();
+	 }
+  }
+}
